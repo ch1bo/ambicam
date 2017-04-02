7206e426d7eeae0fbbf79f678924a895984b0f52
Sebastian Nagel <sebastian.nagel@ncoding.at>
2017-04-02 02:58:56 +0200

    Use findContours and minAreaRect

--- lines.py
+++ lines.py
@@ -9,9 +9,16 @@ cv2.namedWindow('video', cv2.WINDOW_NORMAL)
 cv2.createTrackbar('threshold', 'video', 58, 255, nothing)
 cv2.createTrackbar('cannyLow', 'video', 50, 255, nothing)
 cv2.createTrackbar('cannyHigh', 'video', 150, 255, nothing)
-cv2.createTrackbar('hughThreshold', 'video', 35, 200, nothing)
-cv2.createTrackbar('hughMinLength', 'video', 40, 255, nothing)
-cv2.createTrackbar('hughMaxGap', 'video', 15, 255, nothing)
+
+def longestContour(contours):
+    m = 0
+    longest = None
+    for c in contours:
+        l = cv2.arcLength(c, True)
+        if l > m:
+            m = l
+            longest = c
+    return longest
 
 while True:
     cap = cv2.VideoCapture('test3.mp4')
@@ -24,9 +31,6 @@ while True:
         threshold = cv2.getTrackbarPos('threshold','video')
         cannyLow = cv2.getTrackbarPos('cannyLow','video')
         cannyHigh = cv2.getTrackbarPos('cannyHigh','video')
-        hughThreshold = cv2.getTrackbarPos('hughThreshold','video')
-        hughMinLength = cv2.getTrackbarPos('hughMinLength','video')
-        hughMaxGap = cv2.getTrackbarPos('hughMaxGap','video')
 
         # Detect lines
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -36,22 +40,15 @@ while True:
         edges = cv2.Canny(black, cannyLow, cannyHigh)
         # edges = cv2.Laplacian(black, cv2.CV_8U, 5)
         # lines = cv2.HoughLinesP(edges, 1, numpy.pi / 180, hughThreshold, hughMinLength, hughMaxGap)
+        _, contours, hierarchy = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         # Intersect (naiive) and draw lines
         out = cv2.cvtColor(edges, cv2.COLOR_GRAY2BGR)
-        # corners = []
-        if lines is not None:
-            for line in lines:
-                x1,y1,x2,y2 = line[0]
-                cv2.line(out, (x1,y1), (x2,y2), (0,0,255), 2)
-                for line2 in lines:
-                    x3,y3,x4,y4 = line2[0]
-                    # TODO rewrite using dot and scalar product
-                    denom = (x1-x2)*(y3-y4)-(y1-y2)*(x3-x4)
-                    if denom != 0:
-                        xc = ((x1*y2-y1*x2)*(x3-x4)-(x1-x2)*(x3*y4-y3*x4) / denom)
-                        yc = ((x1*y2-y1*x2)*(y3-y4)-(y1-y2)*(x3*y4-y3*x4) / denom)
-                        # corners.append([xc, yc])
-                        cv2.circle(out, (int(xc),int(yc)), 3, (0,255,0), 2)
+        # cont = longestContour(contours)
+        # hull = cv2.convexHull(cont)
+        cv2.drawContours(out, contours, -1, (0,0,255), 2)
+        rect = cv2.minAreaRect(numpy.vstack(contours))
+        box = numpy.int0(cv2.boxPoints(rect))
+        im = cv2.drawContours(out,[box],0,(0,255,0),2)
 
         fps = cv2.getTickFrequency() / (cv2.getTickCount() - start)
         cv2.putText(out, 'FPS: ' + str(fps), (10,30), cv2.FONT_HERSHEY_PLAIN, 2, (0,255,0), 2)
