fa33fdc4b020f2d26b3d4f5cc72179501abba69e
Sebastian Nagel <sebastian.nagel@ncoding.at>
2017-04-02 19:37:12 +0200

    Multiprocess pipeline for capturing

--- camera.py
+++ camera.py
@@ -2,44 +2,60 @@ import picamera
 import picamera.array
 import cv2
 import time
-import numpy
-import sys
+import numpy as np
+import multiprocessing as mp
+import queue
+import signal
 
-cv2.namedWindow('video', cv2.WINDOW_NORMAL)
-
-with picamera.PiCamera() as camera:
-    with picamera.array.PiYUVArray(camera, size=(640, 480)) as rawCapture:
-        camera.resolution = (640, 480)
-        camera.framerate = 60
-        time.sleep(0.1)
-        start = cv2.getTickCount()
-        for frame in camera.capture_continuous(rawCapture, format='yuv', use_video_port=True):
-                yuv = frame.array
-                # color = frame.array
-                # gray = cv2.cvtColor(color, cv2.COLOR_BGR2GRAY)
-                # ret, black = cv2.threshold(gray, 60, 255, cv2.THRESH_BINARY)
-                # if not ret:
-                #         continue
-                # edges = cv2.Canny(black, 50, 150)
-                # #edges = cv2.Laplacian(black, cv2.CV_8U, 5)
-                # # lines = cv2.HoughLinesP(edges, 1, numpy.pi / 180, hughThreshold, hughMinLength, hughMaxGap)
-                # _, contours, hierarchy = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-                # if not contours:
-                #         continue
-                # #out = cv2.cvtColor(edges, cv2.COLOR_GRAY2BGR)
-                # # cont = longestContour(contours)
-                # # hull = cv2.convexHull(cont)
-                # #cv2.drawContours(color, contours, -1, (0,0,255), 2)
-                # rect = cv2.minAreaRect(numpy.vstack(contours))
-                # box = numpy.int0(cv2.boxPoints(rect))
-                # #im = cv2.drawContours(out,[box],0,(0,255,0),2)
-
-                cv2.imshow('frame', yuv)
-                rawCapture.truncate(0)
+def capture(q, stop, resolution=(640,480), framerate=30):
+    print('Start capturing...')
+    with picamera.PiCamera(resolution=resolution, framerate=framerate) as camera:
+        with picamera.array.PiRGBArray(camera, size=resolution) as raw:
+            start = cv2.getTickCount()
+            for frame in camera.capture_continuous(raw, format="bgr", use_video_port=True):
+                raw.truncate(0)
+                try:
+                    q.put(frame.array, False)
+                except queue.Full:
+                    print('capture: full')
                 fps = cv2.getTickFrequency() / (cv2.getTickCount() - start)
-                print(fps)
+                print('capture: ' + str(fps))
                 start = cv2.getTickCount()
-                if cv2.waitKey(1) & 0xFF == ord('q'):
-                    break
+                if stop.is_set():
+                    return
 
-cv2.destroyAllWindows()
+def process(q, stop):
+    print('Start processing...')
+    while not stop.is_set():
+        start = cv2.getTickCount()
+        try:
+            frame = q.get(False)
+        except queue.Empty:
+            print('process: empty')
+            time.sleep(0.1)
+        else:
+            #cv2.imshow('video', frame)
+            time.sleep(0.03) # simulate 30ms processing
+            fps = cv2.getTickFrequency() / (cv2.getTickCount() - start)
+            print('process: ' + str(fps))
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+    cv2.destroyAllWindows()
+
+def main():
+    q = mp.Queue(10)
+    stop = mp.Event()
+    def sigint(signal, frame):
+        stop.set()
+    signal.signal(signal.SIGINT, sigint)
+
+    p = mp.Process(target=capture, args=(q, stop))
+    p.start()
+    process(q, stop)
+
+    print('Shutdown')
+    stop.set()
+    # p.join()
+
+if __name__ == "__main__":
+    main()
