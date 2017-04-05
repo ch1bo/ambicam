ba2128ae5f6f61449725bd6fc9f3d76fb080e957
Sebastian Nagel <sebastian.nagel@ncoding.at>
2017-04-05 23:21:07 +0200

    Used 320,240 warp image

--- ambicam.py
+++ ambicam.py
@@ -14,6 +14,7 @@ PORT = 19444
 RESOLUTION = (640,480)
 FRAMERATE = 10
 M = np.load('M.npy')
+res = np.load('res.npy')
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
@@ -22,54 +23,44 @@ with picamera.PiCamera(resolution=RESOLUTION, framerate=FRAMERATE) as camera:
         time.sleep(2)
         start = cv2.getTickCount()
         for frame in camera.capture_continuous(raw, format="bgr", use_video_port=True):
-            warped = cv2.warpPerspective(frame.array, M, RESOLUTION)
+            width = res[0]
+            height = res[1]
+            offset = 5
+            warped = cv2.warpPerspective(frame.array, M, (width,height))
             # TODO use configured areas from hypercon
             colors = []
             # bottom center -> bottom left
             for i in range(19):
-                col = int(320-i*310/19)
-                colors.append(int(warped[470][col][2])) # r
-                colors.append(int(warped[470][col][1])) # g
-                colors.append(int(warped[470][col][0])) # b
+                col = int((width/2)-i*(width/2-offset)/19)
+                colors.append(int(warped[width-2*offset][col][2])) # r
+                colors.append(int(warped[width-2*offset][col][1])) # g
+                colors.append(int(warped[width-2*offset][col][0])) # b
             # bottom left -> top left
             for i in range(20):
-                row = int(470-i*460/20)
-                colors.append(int(warped[row][10][2])) # r
-                colors.append(int(warped[row][10][1])) # g
-                colors.append(int(warped[row][10][0])) # b
+                row = int((height-offset)-i*(height-2*offset)/20)
+                colors.append(int(warped[row][offset][2])) # r
+                colors.append(int(warped[row][offset][1])) # g
+                colors.append(int(warped[row][offset][0])) # b
             # top left -> top right
             for i in range(36):
-                col = int(10+i*620/36)
-                colors.append(int(warped[10][col][2])) # r
-                colors.append(int(warped[10][col][1])) # g
-                colors.append(int(warped[10][col][0])) # b
+                col = int(offset+i*(width-2*offset)/36)
+                colors.append(int(warped[offset][col][2])) # r
+                colors.append(int(warped[offset][col][1])) # g
+                colors.append(int(warped[offset][col][0])) # b
             # top left -> bottom left
             for i in range(20):
-                row = int(10+i*460/20)
-                colors.append(int(warped[row][10][2])) # r
-                colors.append(int(warped[row][10][1])) # g
-                colors.append(int(warped[row][10][0])) # b
+                row = int(offset+i*(height-2*offset)/20)
+                colors.append(int(warped[row][offset][2])) # r
+                colors.append(int(warped[row][offset][1])) # g
+                colors.append(int(warped[row][offset][0])) # b
             # bottom right -> bottom center
             for i in range(17):
-                col = int(630-i*310/17)
-                colors.append(int(warped[470][col][2])) # r
-                colors.append(int(warped[470][col][1])) # g
-                colors.append(int(warped[470][col][0])) # b
+                col = int((width-offset)-i*(width/2-offset)/17)
+                colors.append(int(warped[width-2*offset][col][2])) # r
+                colors.append(int(warped[width-2*offset][col][1])) # g
+                colors.append(int(warped[width-2*offset][col][0])) # b
             # rgb = cv2.cvtColor(warped, cv2.COLOR_BGR2RGB)
             print('converted: ' + str((cv2.getTickCount() - start) / cv2.getTickFrequency()))
-            # data = json.dumps({
-            #     'command': 'image',
-            #     'priority': PRIORITY,
-            #     'imageheight': rgb.shape[0],
-            #     'imagewidth': rgb.shape[1],
-            #     'imagedata': base64.b64encode(rgb.tobytes()).decode('ascii')
-            # }) + '\n'
-            # data = b'{"command":"image",' + \
-            #        b'"priority":' + str(PRIORITY).encode('utf-8') + b',' \
-            #        b'"imageheight":' + str(rgb.shape[0]).encode('utf-8') + b',' \
-            #        b'"imagewidth":' + str(rgb.shape[1]).encode('utf-8') + b',' \
-            #        b'"imagedata":"' + base64.b64encode(rgb.tobytes()) + b'"' + \
-            #        b'}' + '\n'.encode('utf-8')
             data = (json.dumps({
                 'command': 'color',
                 'priority': PRIORITY,
